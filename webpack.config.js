d0331cb5efaad7acf0f1055746cc7fbbcce700b1
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-06-30 11:54:17 +0300

    add corrections

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                silenceDeprecations: ["legacy-js-api"],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
